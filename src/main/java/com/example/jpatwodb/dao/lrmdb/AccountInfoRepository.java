c68c410afe00281102dc35834b437d0cb92ddb76
testlrm <testlrm@gmail.com>
2022-09-24 14:32:20 +0800

    remove unuse package

--- src/main/java/com/example/jpatwodb/dao/lrmdb/AccountInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jpatwodb.dao.lrmdb;
-
-import com.example.jpatwodb.model.lrmdb.AccountInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountInfoRepository extends JpaRepository<AccountInfo, String> {
-}
